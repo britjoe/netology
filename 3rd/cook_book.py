a18ec98e47dc9accf9fb18897b2d3bdb3f0c7d2a
YUDKIN Pavel <pavel.yudkin@raiffeisen.ru>
2021-05-18 13:23:36 +0300

    3rd

---
+++ 3rd/cook_book.py
@@ -0,0 +1,42 @@
+cook_book = {
+    'салат': [
+        {'ingridient_name': 'сыр', 'quantity': 50, 'measure': 'гр'},
+        {'ingridient_name': 'томаты', 'quantity': 2, 'measure': 'шт'},
+        {'ingridient_name': 'огурцы', 'quantity': 20, 'measure': 'гр'},
+        {'ingridient_name': 'маслины', 'quantity': 10, 'measure': 'гр'},
+        {'ingridient_name': 'оливковое масло', 'quantity': 20, 'measure': 'мл'},
+        {'ingridient_name': 'салат', 'quantity': 10, 'measure': 'гр'},
+        {'ingridient_name': 'перец', 'quantity': 20, 'measure': 'гр'}
+    ],
+    'пицца': [
+        {'ingridient_name': 'сыр', 'quantity': 20, 'measure': 'гр'},
+        {'ingridient_name': 'колбаса', 'quantity': 30, 'measure': 'гр'},
+        {'ingridient_name': 'бекон', 'quantity': 30, 'measure': 'гр'},
+        {'ingridient_name': 'оливки', 'quantity': 10, 'measure': 'гр'},
+        {'ingridient_name': 'томаты', 'quantity': 20, 'measure': 'гр'},
+        {'ingridient_name': 'тесто', 'quantity': 100, 'measure': 'гр'},
+    ],
+    'лимонад': [
+        {'ingridient_name': 'лимон', 'quantity': 1, 'measure': 'шт'},
+        {'ingridient_name': 'вода', 'quantity': 200, 'measure': 'мл'},
+        {'ingridient_name': 'сахар', 'quantity': 10, 'measure': 'гр'},
+        {'ingridient_name': 'лайм', 'quantity': 20, 'measure': 'гр'},
+    ]
+}
+
+cnt = int(input('Введите количество порций: '))
+
+temp = []
+result = []
+for meal, rec in cook_book.items():
+    temp = temp + rec
+    
+temp_2 = dict()
+for product in temp:
+    ingridient_name, quantity, measure = product.values()
+    if temp_2.get((ingridient_name, measure)) is None:
+        temp_2.update({(ingridient_name, measure): quantity})
+    else:
+        temp_2[(ingridient_name, measure)] += quantity
+for item in temp_2:
+    print(f'{item[0].capitalize()}: {temp_2.get(item)} {item[1]}')
--- 3rd/superDict.py
+++ 3rd/superDict.py
@@ -1,9 +1,16 @@
 my_list = ['a', 'b', 'c', 'd', 'e', 'f']
-my_dict = {'a': {'b': {'c': {'d': {'e': 'f'}}}}}
 
-result = dict()
-
-for i in range(len(my_list)-1, -1, -1):
-    result[1] = i
+my_list.reverse()
+prev_val = None
+for i in my_list:
+    temp_dict = dict()
+    result = dict()
+    if prev_val is None:
+        prev_val = i
+    else:
+        temp_dict.update({i: prev_val})
+        prev_val = temp_dict.copy()
+        result.clear()
+        result.update({i: temp_dict.get(i)})
 
 print(result)
